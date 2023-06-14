d3b144b07b355d773d25796ef5f12b55561752de
Abogeerick <abogeerick@gmail.com>
2023-06-14 09:30:58 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// writeCards() implementation
+const writeCards = (names, occasion) => {
+    const messages = [];
+    names.forEach((name) => {
+      const message = `Thank you, ${name}, for the wonderful ${occasion} gift!`;
+      messages.push(message);
+    });
+    return messages;
+  };
+  
+  // countDown() implementation
+  const countDown = (number) => {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  };
+  
+  module.exports = {
+    writeCards,
+    countDown
+  };
+  
